5ce80e193642afee87d8a2aec4a2537e68201997
IbrahimEmpire <110913184+IbrahimEmpire@users.noreply.github.com>
2022-08-31 19:59:20 +0530

    Add files via upload

---
+++ day6oops.js
@@ -0,0 +1,21 @@
+
+// day 6 oops task 
+//  Write a “person” class to hold all the details.
+
+class student {
+    constructor(Name,SchoolName, Age, Study, Home, Grade, Spots){
+        this.name = Name;
+        this.schoolname = SchoolName;
+        this.age = Age;
+        this.study = Study;
+        this.home = Home;
+        this.grade = Grade;
+        this.spots = Spots;
+    }
+}
+const Cat_valentine = new student("Cat valentine","Hollywood atrs",22,"Flim Director","Florida US","a+","Kabadi" )
+ const Tory_vega = new student("Tory_vega","Hollywood atrs",25,"Flim Director","Florida US","C","Running" )
+const Jade = new student("Jade","Hollywood atrs",25,"Flim Director","Germen","C","scissor Cutting" )
+console.log(Cat_valentine)
+console.log(Tory_vega)
+console.log(Jade)
